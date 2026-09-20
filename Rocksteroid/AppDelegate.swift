e7686cb5d36f9caf716e9fa98bbcb97c3e1ee278
Anri <anri.shkliar@gmail.com>
2026-09-20 20:50:19 +0200

    Initial Commit

---
+++ Rocksteroid/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Rocksteroid
+//
+//  Created by Anri Shkliar on 20/09/2026.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ RocksteroidTests/RocksteroidTests.swift
@@ -0,0 +1,19 @@
+//
+//  RocksteroidTests.swift
+//  RocksteroidTests
+//
+//  Created by Anri Shkliar on 20/09/2026.
+//
+
+import Testing
+@testable import Rocksteroid
+
+struct RocksteroidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
